66de96ec0ab34fa28629b68e414f7459e292dc0f
zhouli <zhouli@espressif.com>
2021-10-08 19:34:36 +0800

    add s3-eye board pin

--- main/include/app_camera.h
+++ main/include/app_camera.h
@@ -119,6 +119,26 @@
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
+#elif CONFIG_CAMERA_MODULE_ESP_S3_EYE
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM 7
+#define PCLK_GPIO_NUM 13
+#define XCLK_GPIO_NUM 15
+
+#define SIOD_GPIO_NUM 4
+#define SIOC_GPIO_NUM 5
+
+#define Y2_GPIO_NUM 11
+#define Y3_GPIO_NUM 9
+#define Y4_GPIO_NUM 8
+#define Y5_GPIO_NUM 10
+#define Y6_GPIO_NUM 12
+#define Y7_GPIO_NUM 18
+#define Y8_GPIO_NUM 17
+#define Y9_GPIO_NUM 16
 
 #elif CONFIG_CAMERA_MODEL_CUSTOM
 #define PWDN_GPIO_NUM    CONFIG_CAMERA_PIN_PWDN
